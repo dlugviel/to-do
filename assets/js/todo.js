247cb56e761f53172424cb9b522e717c54d39c2f
dlugviel <dlugviel@email.arizona.edu>
2017-08-04 23:06:57 +0000

    ability to add todos

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,12 +1,22 @@
 // check off specific to dos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 // click on X to delete to do
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grab new todo text
+        var todoText = $(this).val();
+        $(this).val("");
+        // create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 })
